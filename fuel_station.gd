edc46f3ef1a8376cbef5fa8ce1335f9ad91cc7fd
thulasipavankumar <thulasipavankumar@gmail.com>
2026-04-26 14:18:33 +0200

    added fuel station and dialog to buy fuel

--- fuel_station.gd
+++ fuel_station.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$FuelUI.hide()
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("Entered fuel station")
-	pass # Replace with function body.
+	$FuelUI.show()
+	pass
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	$FuelUI.hide()
+	pass 
+
+func _on_button_pressed() -> void:
+	# refill if user has cash 1$ = 1 unit of fuel
+	pass 
