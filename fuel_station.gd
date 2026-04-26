4253753a4c0b40afa96b4d37bfa53b67fb3e183f
thulasipavankumar <thulasipavankumar@gmail.com>
2026-04-26 14:24:31 +0200

    added refill fuel logic with fuel station

--- fuel_station.gd
+++ fuel_station.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+# Holds the player while they are inside the station area.
+var _player = null
+
 func _ready() -> void:
 	$FuelUI.hide()
-	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Entered fuel station")
+	_player = body
 	$FuelUI.show()
-	pass
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
+	_player = null
 	$FuelUI.hide()
-	pass 
 
+# Refills as much fuel as the player can afford at $1 per unit.
 func _on_button_pressed() -> void:
-	# refill if user has cash 1$ = 1 unit of fuel
-	pass 
+	if _player == null:
+		return
+	var fuel_needed: int = _player.maxFuel - _player.currentFuel
+	if fuel_needed <= 0 or _player.cash <= 0:
+		return
+	# Player can only buy as much fuel as they have cash for.
+	var fuel_to_add: int = mini(fuel_needed, _player.cash)
+	_player.spend_cash(fuel_to_add)
+	_player.refill_fuel(fuel_to_add)
