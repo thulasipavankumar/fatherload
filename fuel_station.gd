c16e3c5e54693cc6c40d4705ddbebf96cd26b7cc
thulasipavankumar <thulasipavankumar@gmail.com>
2026-04-26 13:34:25 +0200

    aded fuel station to the scene

--- fuel_station.gd
+++ fuel_station.gd
@@ -10,3 +10,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Entered fuel station")
+	pass # Replace with function body.
